57ff51bdc1410221a343cfe549418ea99ff2d548
M. Raka Maulana A <rmalan09@outlook.com>
2020-10-19 06:03:43 +0700

    Update asset utk mode offline

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -4,7 +4,14 @@ import CacheHelper from './utils/cache-helper';
 const { assets } = global.serviceWorkerOption;
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
+  event.waitUntil(
+    CacheHelper.cachingAppShell([
+      ...assets,
+      './',
+      'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
+      'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.css',
+    ]),
+  );
 });
 
 self.addEventListener('activate', (event) => {
